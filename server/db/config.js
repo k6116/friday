9c1f518c5f2d3da1dfd3a228906d4ee5712b6b45
SCHUETZLE <bill_schuetzle@keysight.com>
2018-03-07 08:28:24 -0800

    added server files

---
+++ server/db/config.js
@@ -0,0 +1,15 @@
+
+const config = {
+  dbname: "Jarvis_DEV",
+  host: 'SPYGLASS.SRS.IS.KEYSIGHT.COM',
+  username: 'JarvisRTUser',
+  password: '3pineapple#',
+  dialect: 'mssql'
+};
+
+
+module.exports = {
+  config: config
+};
+
+
---
+++ server/routes/api.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+
+var controllers = require('../controllers/_index.js');
+
+
+
+module.exports = router;
