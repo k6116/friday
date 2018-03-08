23de5472cb103590c3fbe6a3d242ad97a17f7a66
SCHUETZLE <bill_schuetzle@keysight.com>
2018-03-08 14:13:13 -0800

    added route to main component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
+import { MainComponent } from './main/main.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'main', component: MainComponent },
   { path: '**', redirectTo: '/login' }
 ];
+
 
 @NgModule({
   imports: [
