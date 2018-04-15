c2259943ed6c8d1ffc2104a438163f3ec35a9847
SCHUETZLE <bill_schuetzle@keysight.com>
2018-04-14 17:26:04 -0700

    added initial content and code to top nav bar

--- src/app/navs/top-nav/top-nav.component.ts
+++ src/app/navs/top-nav/top-nav.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppDataService } from '../../_shared/services/app-data.service';
+
+declare var $: any;
 
 @Component({
   selector: 'app-top-nav',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private appDataService: AppDataService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onAppClick() {
+    window.location.href = '/main';
+  }
+
+  onMenuLinkClick(menu: string) {
+    console.log('menu item clicked: ' + menu);
+    // this.appDataService.partNumber = undefined;
+    // this.appDataService.recipe = undefined;
+    // this.appDataService.selectedMenu = menu;
+    $('.app-menu-links').css('display', 'none');
+    setTimeout(() => {
+      $('.app-menu-links').css('display', '');
+    }, 100);
+    this.router.navigate([`/${menu}`]);
   }
 
 }
