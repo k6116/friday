fec923c2ce1c1cbc024f7c4518174b1004af3134
SCHUETZLE <bill_schuetzle@keysight.com>
2018-04-14 17:55:42 -0700

    added initial content and code to side nav

--- src/app/navs/side-nav/side-nav.component.ts
+++ src/app/navs/side-nav/side-nav.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AppDataService } from '../../_shared/services/app-data.service';
+
+declare var $: any;
 
 @Component({
   selector: 'app-side-nav',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavComponent implements OnInit {
 
-  constructor() { }
+  selectedMenu: string;
+  toggleMode: string;
+  expandCollapseTooltip: string;
+  initStart: number;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private appDataService: AppDataService) { }
 
   ngOnInit() {
+
+    // init the tooltip javascript (still need this for the expand/collapse since it is applied in the html)
+    $('[data-toggle="tooltip"]').tooltip({ trigger: 'hover' });
+
+    // get the current route path from the url
+    const path = this.router.url.slice(1, this.router.url.length);
+
+    // set the icon color based to light blue for the current/active menu
+    // needed here if the user goes directly to the route using the url
+    this.selectedMenu = path;
+
   }
 
 }
