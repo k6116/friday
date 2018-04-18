95ef5f1072b373224c5e474bd12f40a0b876bc36
SCHUETZLE <bill_schuetzle@keysight.com>
2018-04-18 12:42:43 -0700

    added comments for side nav and top nav code

--- src/app/navs/side-nav/side-nav.component.ts
+++ src/app/navs/side-nav/side-nav.component.ts
@@ -24,9 +24,7 @@ export class SideNavComponent implements OnInit, AfterViewInit {
     private activatedRoute: ActivatedRoute,
     private appDataService: AppDataService) {
 
-      // on change (expand or collapse), update array of main menu items with expanded property
-      // and set in app-data.service
-      // on init, pick up the data and update the menu structure while using jquery to set the heights, temporarily removing transitions
+      // build an object that represents the sidebar menu structure, to be rendered in the html
       this.menuStructure = [
         {
           title: 'FTE Entry',
@@ -90,6 +88,8 @@ export class SideNavComponent implements OnInit, AfterViewInit {
 
     // get the array of expanded menu objects from the cache
     // if the expandedMenus have data, call a method to expand the appropriate ones
+    // this is because on navigation this component will always be reinitialized
+    // TO-DO: hopefully this can be avoided by restructuring the app with child and auxilary routing
     this.expandedMenus = this.appDataService.expandedMenus;
     if (this.expandedMenus) {
       if (this.expandedMenus.length) {
@@ -97,15 +97,16 @@ export class SideNavComponent implements OnInit, AfterViewInit {
       }
     }
 
-    // get the current route path from the url
+    // get the current route path from the url e.g. reports/projects, fte-entry/team, etc.
     const path = this.router.url.slice(1, this.router.url.length);
 
-    // set the icon color based to light blue for the current/active menu
+    // highlight the selected/active menu item with color, background color, etc.
     // needed here if the user goes directly to the route using the url or on refresh
     this.selectedMenu = path;
     this.highlightActiveMenu(path);
 
-    // get parent menu object based on the current path
+    // attempt to find the parent menu item, if a sub-menu item is the active one
+    // if one is found, expand it so that the active sub-menu can be seen
     this.getParentOfCurrentRoute(path);
     if (this.parentMenuToExpand) {
       const expandedMenu = [];
@@ -117,14 +118,21 @@ export class SideNavComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
 
+    // NOTE: the actual expanding of main menu items must be done in this lifecycle hook,
+    // because the html won't be rendered yet in the init hook
+    // what is done on init is just setting the properties
+    // separating these two is also used to avoid this error for the class binding for the caret icon:
+    // Error: ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked
+
+    // expand the main menu items identified on init if any
     if (this.expandedMenus) {
       if (this.expandedMenus.length) {
-        console.log('expanding menus');
         this.expandMenus(this.expandedMenus);
       }
+    // expand the parent main menu item idenfied on init if any
     } else if (this.parentMenuToExpand) {
-      console.log('expanding parent menu');
       const expandedMenu = [];
+      // push it into an array so that the expandMenus method can be reused
       expandedMenu.push(this.parentMenuToExpand);
       this.expandMenus(expandedMenu);
     }
@@ -132,16 +140,18 @@ export class SideNavComponent implements OnInit, AfterViewInit {
   }
 
   onMenuItemClick(menuItem: string) {
-    console.log(`menu item clicked: ${menuItem}`);
+    // get the element object using jQuery
     const $el = $(`div.sidenav-menu-item.${menuItem}`);
+    // get the menu item object in the menu structure (this.menuStructure)
     const foundMenuItem = this.getMenuObject(menuItem);
     if (foundMenuItem) {
-      // if the menu item has a sub-menu, expand or collapse the menu
+      // if the menu item has a sub-menu, expand or collapse the menu (toggle it by passing !foundMenuItem.expanded)
       if (foundMenuItem.subItems) {
         this.expandOrCollapseMenu(!foundMenuItem.expanded, menuItem, true);
+        // store the expanded menus in the cache (app-data service) so they can be opened up on re-init
         this.storeExpandedMenus();
       } else {
-        // store the active/selected menu item in the cache (app-data service)
+        // store the active/selected menu item in the cache (app-data service) so it can be highlighed on re-init
         this.appDataService.selectedMenu = menuItem;
         // navigate to the selected/clicked route
         this.router.navigate([`/${foundMenuItem.path}`]);
@@ -150,14 +160,15 @@ export class SideNavComponent implements OnInit, AfterViewInit {
   }
 
   onSubMenuItemClick(menuItem: string, subMenuItem: string) {
-    console.log(`sub menu item clicked: ${subMenuItem}`);
+    // get the menu item object in the menu structure (this.menuStructure)
+    // TO-DO: attempt to combine getMenuObject and getSubMenuObject into a single method
     const foundMenuItem = this.getSubMenuObject(menuItem, subMenuItem);
     // navigate to the selected/clicked route
     this.router.navigate([`/${foundMenuItem.path}`]);
   }
 
-  // go through the entire menu structure (main menu and sub menu items)
-  // and set the active property to true if the alias matches (and set all others to false)
+  // hightlight the active/selected menu by going through the entire menu structure (main menu and sub menu items)
+  // and set the active property to true if the path matches (and set all others to false along the way)
   highlightActiveMenu(path: string) {
     this.menuStructure.forEach(menuItem => {
       if (menuItem.hasOwnProperty('path')) {
@@ -171,43 +182,50 @@ export class SideNavComponent implements OnInit, AfterViewInit {
     });
   }
 
+  // get the parent of the selected/active menu item based on the path
+  // used to expand that parent on init
   getParentOfCurrentRoute(path: string) {
+    // loop through each main menu item in the menu structure
     this.menuStructure.forEach(menuItem => {
+      // if the main menu item has a subItems property, attempt to find it within that array
       if (menuItem.hasOwnProperty('subItems')) {
         const foundSubMenuItem = menuItem.subItems.find(subItem => {
           return subItem.path === path;
         });
+        // once the sub menu item is found, find it's parent main menu item (matching using the parentAlias)
         if (foundSubMenuItem) {
           const foundMainMenuItem = this.menuStructure.find(mainItem => {
             return mainItem.alias === foundSubMenuItem.parentAlias;
           });
+          // set the property
+          // TO-DO: make this method return the object instead of setting a class property, would be more proper
           this.parentMenuToExpand = foundMainMenuItem;
         }
       }
     });
   }
 
-  getMenuObject(menuItem: string): any {
+  // find and return a main menu item/object using the alias
+  getMenuObject(alias: string): any {
     return this.menuStructure.find(menu => {
-      return menu.alias === menuItem;
+      return menu.alias === alias;
     });
   }
 
-  getSubMenuObject(menuItem: string, subMenuItem: string): any {
+  // find and return a sub menu item/object using the alias
+  getSubMenuObject(mainMenuAlias: string, subMenuAlias: string): any {
     const foundMenuItem = this.menuStructure.find(menu => {
-      return menu.alias === menuItem;
+      return menu.alias === mainMenuAlias;
     });
     if (foundMenuItem) {
       return foundMenuItem.subItems.find(subMenu => {
-        return subMenu.alias === subMenuItem;
+        return subMenu.alias === subMenuAlias;
       });
     }
   }
 
-  getMenuElement(alias: string): any {
-    return $(`div.sidenav-menu-item.${alias}`);
-  }
-
+  // go through the menu structure (main menu items only) and find the ones that are expanded
+  // store this array of objects (returned using the filter method) in the cache / app-data service
   storeExpandedMenus() {
     const expandedMenus = this.menuStructure.filter(menu => {
       return menu.expanded;
@@ -215,6 +233,7 @@ export class SideNavComponent implements OnInit, AfterViewInit {
     this.appDataService.expandedMenus = expandedMenus;
   }
 
+  // go through the cached expanded menu items and set the expanded property to true
   setExpandedProperties(expandedMenus: any) {
     expandedMenus.forEach(expandedMenu => {
       const foundMenuItem = this.menuStructure.find(menuItem => {
@@ -226,6 +245,7 @@ export class SideNavComponent implements OnInit, AfterViewInit {
     });
   }
 
+  // go through the cached expanded menu items and expand them, without any animation/transition
   expandMenus(expandedMenus: any) {
     expandedMenus.forEach(expandedMenu => {
       const foundMenuItem = this.menuStructure.find(menuItem => {
@@ -235,20 +255,28 @@ export class SideNavComponent implements OnInit, AfterViewInit {
         this.expandOrCollapseMenu(true, foundMenuItem.alias, false, true);
       }
     });
+    // need to call this for edge case where the page is refreshed at a sub menu route, and the parent is then expanded to show it
+    // without this, a click on a different menu item and subsequent re-route would collapse it
     this.storeExpandedMenus();
   }
 
+  // method to perform the expanding or collapsing of a main menu item
   expandOrCollapseMenu(expand: boolean, alias: string, animate: boolean, skipPropertyUpdate?: boolean) {
+    // ge the element using jQuery and the alias which will be included in the element classes
     const $el = $(`div.sidenav-menu-item.${alias}`);
+    // find the menu item using the alias
     const foundMenuItem = this.getMenuObject(alias);
+    // set/calculate the height - will be 55 pixels collapses, and 55 + 40 times the number of subitems (and extra 3px)
     const height = expand ? 55 + 3 + (foundMenuItem.subItems.length * 40) : 55;
+    // if animation is desired, set the transition css otherwise clear it
     if (animate) {
       $el.css('transition', 'height .35s ease-out');
     } else {
       $el.css('transition', '');
     }
+    // set the height using jQuery css method
     $el.css('height', `${height}px`);
-    // NOTE: skipPropertyUpdate is used to avoid error due to init vs. afterview init lifecycle conflic
+    // NOTE: skipPropertyUpdate is used to avoid error due to init vs. afterview init lifecycle conflict
     // Error: ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked
     if (!skipPropertyUpdate) {
       foundMenuItem.expanded = expand;
--- src/app/navs/top-nav/top-nav.component.ts
+++ src/app/navs/top-nav/top-nav.component.ts
@@ -12,6 +12,7 @@ declare var $: any;
   selector: 'app-top-nav',
   templateUrl: './top-nav.component.html',
   styleUrls: ['./top-nav.component.css'],
+  // TO-DO: figure out why the void to in transition doesn't seem to be working
   animations: [
     trigger('conditionState', [
       state('in', style({
@@ -47,7 +48,6 @@ export class TopNavComponent implements OnInit, OnDestroy {
 
     // TO-DO: try to find a simpler/cleaner way to hide the dropdown on click outside
     // NOTE: ng4 click outside will only work outside (but within) the navbar component
-
     this.showDropDown = false;
 
   }
@@ -55,21 +55,27 @@ export class TopNavComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
 
+    // get the logged in user object from the auth service
+    // most of the time, this will be stored in the cache when navigating around the app
     this.authService.getLoggedInUser((user, err) => {
       if (err) {
         console.log(`error getting logged in user: ${err}`);
         return;
       }
       this.loggedInUser = user;
+      // get the user's first name initial to create the google style 'avatar'
       this.firstInitial = this.loggedInUser.fullName.substring(0, 1).toUpperCase();
     });
 
+    // subscribe to the clickedClass property, to determine if the dropdown container should be closed if it is open
     this.subscription1 = this.appDataService.clickedClass.subscribe(
       (clickedClass: string) => {
         this.hideDropDown(clickedClass);
     });
 
+    // set state to in to enable the angular animations
     this.state = 'in';
+
   }
 
 
@@ -77,17 +83,19 @@ export class TopNavComponent implements OnInit, OnDestroy {
     this.subscription1.unsubscribe();
   }
 
-
+  // when the identity area is clicked (logo and text), refresh the page directed to the main route
   onAppClick() {
     window.location.href = '/main';
   }
 
-
+  // when the avatar icon is clicked, toggle the property which will either show or hide it using *ngIf
   onAvatarClick() {
     this.showDropDown = !this.showDropDown;
   }
 
-
+  // hide the drop down container if the clicked class is not one inside the container
+  // note: this is the current 'click outside' solution,
+  // that will close it if the user clicks anywhere else other than in the container itself
   hideDropDown(clickedClass: string) {
     if (this.showDropDown) {
       if (clickedClass.split(' ')[0] !== 'topnav-menu') {
@@ -98,12 +106,13 @@ export class TopNavComponent implements OnInit, OnDestroy {
 
 
   onProfileButtonClick() {
+    // TEMP CODE: log the click to test the button
     console.log('profile button clicked');
   }
 
 
   onLogoutButtonClick() {
-    // log the user out, don't show auto-logout message
+    // log the user out and don't show auto-logout message by passing in false
     this.authService.routeToLogin(false);
   }
 
