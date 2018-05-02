16e6a983ea4f929ee510bdbe3ac072f0b3d5c89d
SCHUETZLE <bill_schuetzle@keysight.com>
2018-05-02 14:55:27 -0700

    added filter service file in _shared

---
+++ src/app/_shared/services/filter.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class FilterService {
+
+  constructor() { }
+
+  filter(objects: any[], filter: any, property: string, options?: any): any {
+
+  }
+
+}
