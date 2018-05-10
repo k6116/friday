5792aaaf90cc9d6774e0c3da5e4c73ccdebb0c04
Bryan Cheung <bryan.cheung@keysight.com>
2018-05-10 16:49:40 -0700

    added cleanup for toast subscription

--- src/app/toast/toast.component.ts
+++ src/app/toast/toast.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AppDataService } from '../_shared/services/app-data.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-toast',
   templateUrl: './toast.component.html',
   styleUrls: ['./toast.component.css']
 })
-export class ToastComponent implements OnInit {
+export class ToastComponent implements OnInit, OnDestroy {
 
   constructor(private appDataService: AppDataService) { }
 
+  toastSubscription: Subscription;
   toastType: string;
   toastText: string;
   toastShow = false;
 
   ngOnInit() {
-    this.appDataService.toast.subscribe( toast => {
+    this.toastSubscription = this.appDataService.toast.subscribe( toast => {
       this.toastType = `toast toast-${toast.type}`;
       this.toastText = toast.text;
       this.showToast();
@@ -26,5 +28,9 @@ export class ToastComponent implements OnInit {
     // show the toast, then remove the toastShow class from the div after 3 sec
     this.toastShow = true;
     setTimeout(() => { this.toastShow = false; }, 3000);
+  }
+
+  ngOnDestroy() {
+    this.toastSubscription.unsubscribe();
   }
 }
