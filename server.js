c5adef0952b83f9c1291e7904b59b3ad641b0d4d
SCHUETZLE <bill_schuetzle@keysight.com>
2018-05-16 16:03:37 -0700

    added etc directory and files

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ const sequelize = require('./server/db/sequelize');
 
 // set the ssl options object, reading keys and certs from the filesystem
 // NOTE: for use on the web server uncomment this block
-// var sslOptions = {
-//   key: fs.readFileSync('./etc/ssl/jarvis.key'),
-//   cert: fs.readFileSync('./etc/ssl/jarvis.crt'),
-//   requestCert: true,
-//   ca: [
-//     fs.readFileSync('./etc/ssl/Keysight_Intermediate.crt'),
-//     fs.readFileSync('./etc/ssl/Keysight_Root.crt')
-//   ],
-//   rejectUnauthorized: false 
-// };
+var sslOptions = {
+  key: fs.readFileSync('./etc/ssl/jarvis.key'),
+  cert: fs.readFileSync('./etc/ssl/jarvis.crt'),
+  requestCert: true,
+  ca: [
+    fs.readFileSync('./etc/ssl/Keysight_Intermediate.crt'),
+    fs.readFileSync('./etc/ssl/Keysight_Root.crt')
+  ],
+  rejectUnauthorized: false 
+};
 
 // connect to the database(s)
 sequelize.connect();
