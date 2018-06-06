2f649e2777dc4a10265f6940ae4e55e819c94898
SCHUETZLE <bill_schuetzle@keysight.com>
2018-06-05 19:08:57 -0700

    installed socket.io

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  styleUrls: ['./test.component.css', '../_shared/styles/common.css']
 })
 export class TestComponent implements OnInit {
 
