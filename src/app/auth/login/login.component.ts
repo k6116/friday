123da7fd1016b3eb083249de191163f6fde27b57
SCHUETZLE <bill_schuetzle@keysight.com>
2018-07-05 19:41:34 -0700

    updated login button (centered animation, disabled button)

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -275,13 +275,13 @@ export class LoginComponent implements OnInit, OnDestroy {
   // display error message and icon for missing username or password, also set focus for the user for convenience
   displayFormEntryErrors() {
     if (!this.userName && !this.password) {
-      this.displayMessage('Please enter your user name and password', 'fa-exclamation-triangle', 'rgb(139, 0, 0)');
+      this.displayMessage('Please enter your user name and password', 'fa-exclamation-triangle', 'rgb(255, 193, 7)');
       this.userNameVC.nativeElement.focus();
     } else if (!this.userName) {
-      this.displayMessage('Please enter your user name', 'fa-exclamation-triangle', 'rgb(139, 0, 0)');
+      this.displayMessage('Please enter your user name', 'fa-exclamation-triangle', 'rgb(255, 193, 7)');
       this.userNameVC.nativeElement.focus();
     } else if (!this.password) {
-      this.displayMessage('Please enter your password', 'fa-exclamation-triangle', 'rgb(139, 0, 0)');
+      this.displayMessage('Please enter your password', 'fa-exclamation-triangle', 'rgb(255, 193, 7)');
       this.passwordVC.nativeElement.focus();
     }
   }
@@ -314,8 +314,8 @@ export class LoginComponent implements OnInit, OnDestroy {
       }
     // otherwise, this should be a failed login (invalid credentials)
     } else {
-      this.displayMessage('Invalid user name or password.  Note: Use your windows credentials to login.',
-        'fa-exclamation-triangle', 'rgb(139, 0, 0)');
+      this.displayMessage('Invalid user name or password.  Note: Use your Windows credentials to login.',
+        'fa-exclamation-triangle', 'rgb(255, 193, 7)');
     }
   }
 
