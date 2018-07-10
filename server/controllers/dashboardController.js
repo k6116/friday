ca9632fa28a6feb21854c96b4d1864aa32b4f683
SCHUETZLE <bill_schuetzle@keysight.com>
2018-07-10 10:17:13 -0700

    added dashboard controller

---
+++ server/controllers/dashboardController.js
@@ -0,0 +1,13 @@
+
+const models = require('../models/_index');
+const sequelize = require('../db/sequelize').sequelize;
+const Sequelize = require('sequelize')
+const moment = require('moment');
+// const sequelizePLM = require('../db/sequelize').sequelizePLM;
+// const sequelize = require('../db/sequelize').sequelize2017;
+
+
+
+module.exports = {
+
+}
