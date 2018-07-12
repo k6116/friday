914e8f328c828d9522a5116f1d1f1fbe53606153
SCHUETZLE <bill_schuetzle@keysight.com>
2018-07-12 09:25:21 -0700

    added comments to click tracking service

--- src/app/_shared/services/click-tracking.service.ts
+++ src/app/_shared/services/click-tracking.service.ts
@@ -17,47 +17,60 @@ export class ClickTrackingService {
   ) { }
 
 
-  // TO-DO BILL: add more comments, in particular how to implement at the top
 
-  logClickWithEvent(clickTrack: string) {
-    this.logClick(clickTrack);
-  }
+  // usage: two options:
+  // 1. add a custom attribute named data-clicktrack to the element that you want to track a click for, such as a button, div, etc.
+  // it should specify the page, clicked on element, and text if any.  variables can be mixed in.  examples:
+  // [attr.data-clicktrack]="'page: Login, clickedOn: Login Button, text: ' + userName"
+  // [attr.data-clicktrack]="'page: Main, clickedOn: Menu Item > ' + menu.title + ' > ' + subMenu.title"
+  // for divs, always apply the attribute to the outermost element, since an inner element such as an icon or text might actually be clicked
+  // 2. if the custom attribute cannot be used for whatever reason and it must be triggered from the code (typescript),
+  // you can use the logClickWithEvent method; passing the string in the same format
+  // with page, clickedOn, and text (optional).  example:
+  // this.clickTrackingService.logClickWithEvent(`page: Login, clickedOn: Login Button, text: ${this.userName}`);
 
 
+  // log the click with the custom data-clicktrack attribute from html
   logClickWithAttribute(event: Event) {
 
     // get the element into a jQuery object
     const $el = $(event.target);
 
-    // get the data from the attribute
-    // explain what closest does and especially why
+    // get the data from the attribute.
+    // closest is a jQuery method that will traverse up the DOM tree and find the first element
+    // that matches the selector, in this case ('[data-clicktrack]'), starting with the element itself
+    // this is needed if an inner element such as text or an icon was clicked instead of the element that has the attribute
     const clickTrack = $el.closest('[data-clicktrack]').data('clicktrack');
 
-    // if any data was found
+    // if any data was found, log the click
     if (clickTrack) {
       this.logClick(clickTrack);
     }
 
   }
 
+  // log the click using the string passed from code
+  logClickWithEvent(clickTrack: string) {
+    this.logClick(clickTrack);
+  }
+
 
   logClick(clickTrack: string) {
 
     // get the user id
-    // TO-DO BILL: don't need the ternary, since loggedInUser will always be there now with the resolver
-    const userID = this.authService.loggedInUser ? this.authService.loggedInUser.id : null;
+    const userID = this.authService.loggedInUser.id;
 
     // get the url path
     const path = this.router.url;
 
     // build an object that will be inserted into the table
+    // NOTE: for now, page, clickedOn, and text will be null, and will be updated later with the clickTrack string
     const clickObj = {
       clickedDateTime: moment(),
       employeeID: userID,
       page: null,
       path: path,
       clickedOn: null,
-      clickedOnSub: null,
       text: null,
       browser: bowser.name,
       browserVersion: bowser.version,
@@ -67,9 +80,13 @@ export class ClickTrackingService {
 
     // replace the page, clickedOn, and text property values if found in the attribute string
     let error = false;
+    // split the string into an array of key value pairs formatted like key: value
     const clickTrackArr = clickTrack.split(',');
+    // for each key value pair such as page, clickedOn, and text (consider these like objects)
     clickTrackArr.forEach(obj => {
+      // split the key and value into an array of 2
       const objArr = obj.split(':');
+      // get the property
       const propName = objArr[0].trim();
       // if the propname from the html custom attribute matches a property in the object, update the property value
       if (clickObj.hasOwnProperty(propName)) {
@@ -95,7 +112,8 @@ export class ClickTrackingService {
       this.apiDataService.logClick(clickObj, userID)
       .subscribe(
         res => {
-          console.log(res);  // click tracking record was inserted successfully
+          // click tracking record was inserted successfully
+          console.log(res);
         },
         err => {
           console.error(err);
