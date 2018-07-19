81cc66fa5f6f9e2901626bc1e0f666801d2cdc54
Bryan Cheung <bryan.cheung@keysight.com>
2018-07-18 21:28:36 -0700

    adding bom graph component

---
+++ src/app/reports/bom-graph/bom-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bom-graph',
+  templateUrl: './bom-graph.component.html',
+  styleUrls: ['./bom-graph.component.css']
+})
+export class BomGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
