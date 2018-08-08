e7b416da114333e17ae2ef053f93417219005796
SCHUETZLE <bill_schuetzle@keysight.com>
2018-08-08 15:26:41 -0700

    added popover for long project descriptions

--- src/app/projects/browse-projects/browse-projects.component.ts
+++ src/app/projects/browse-projects/browse-projects.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiDataProjectService } from '../../_shared/services/api-data/_index';
 
+declare var $: any;
+
 @Component({
   selector: 'app-browse-projects',
   templateUrl: './browse-projects.component.html',
@@ -10,10 +12,77 @@ export class BrowseProjectsComponent implements OnInit {
 
   projects: any;
   showPage: boolean;
+  projectTypeBinding: any;
 
   constructor(
     private apiDataProjectService: ApiDataProjectService
-  ) { }
+  ) {
+
+    // create an object to use for class and style binding for the project type
+    this.projectTypeBinding = [
+      {
+        projectTypeName: 'NCI',
+        iconClass: 'nc-ram',
+        color: 'darkred'
+      },
+      {
+        projectTypeName: 'NPI',
+        iconClass: 'nc-keyboard-wireless',
+        color: 'darkgreen'
+      },
+      {
+        projectTypeName: 'NPPI',
+        iconClass: 'nc-socket-europe-1',
+        color: 'darkred'
+      },
+      {
+        projectTypeName: 'NTI',
+        iconClass: 'nc-lab',
+        color: 'darkred'
+      },
+      {
+        projectTypeName: 'Research',
+        iconClass: 'nc-microscope',
+        color: 'darkred'
+      },
+      {
+        projectTypeName: 'MFG',
+        iconClass: 'nc-settings-91',
+        color: 'darkred'
+      },
+      {
+        projectTypeName: 'Program',
+        iconClass: 'nc-code-editor',
+        color: 'darkred'
+      },
+      {
+        projectTypeName: 'Solution',
+        iconClass: 'nc-book-open-2',
+        color: 'darkred'
+      },
+      {
+        projectTypeName: 'Initiative',
+        iconClass: 'nc-board-28',
+        color: 'darkred'
+      },
+      {
+        projectTypeName: 'TOF Engaged',
+        iconClass: 'nc-bulb-63',
+        color: 'darkred'
+      },
+      {
+        projectTypeName: 'Completed',
+        iconClass: 'nc-sign-closed',
+        color: 'darkred'
+      },
+      {
+        projectTypeName: 'General',
+        iconClass: 'nc-gantt',
+        color: 'darkred'
+      },
+    ];
+
+  }
 
   ngOnInit() {
 
@@ -25,7 +94,7 @@ export class BrowseProjectsComponent implements OnInit {
           console.log(res);
           this.projects = res;
           // TEMP CODE: trim down the list of projects to a smaller number, for testing css
-          this.trimProjects(10);
+          this.trimProjects(100);
           // display the page
           this.showPage = true;
         },
@@ -38,9 +107,100 @@ export class BrowseProjectsComponent implements OnInit {
   }
 
 
+  // display a popover with the full description
+  onDescriptionMouseEnter(project: any) {
+
+    // console.log(project);
+
+    console.log('project description and length:');
+    console.log(project.Description);
+    console.log(project.Description ? project.Description.length : 0);
+
+    // only show the popover if there is a description (not null) and it is over 200 characters
+    if (project.Description) {
+      if (project.Description.length >= 130) {
+
+        // set the jquery element
+        const $el = $(`div.project-description[data-id="${project.ProjectID}"]`);
+
+        // set the popover options
+        const options = {
+          animation: true,
+          placement: 'top',
+          html: true,
+          trigger: 'focus',
+          title: `Description`,
+          content: project.Description
+        };
+
+        // show the popover
+        $el.popover(options);
+        $el.popover('show');
+
+      }
+
+    }
+
+  }
+
+
+  // hide/destroy the popover
+  onDescriptionMouseLeave(projectID: number) {
+
+    // set the jquery element
+    const $el = $(`div.project-description[data-id="${projectID}"]`);
+
+    // dispose of the popover
+    $el.popover('dispose');
+
+  }
+
+
 
   trimProjects(numProjects: number) {
     this.projects = this.projects.slice(0, numProjects);
   }
+
+
+  setProjctTypeIconClass(projectTypeName) {
+    const classes = {
+      'nc-icon': true,
+      'nc-ram': projectTypeName === 'NCI',
+      'nc-keyboard-wireless': projectTypeName === 'NPI',
+      'nc-socket-europe-1': projectTypeName === 'NPPI',
+      'nc-lab': projectTypeName === 'NTI',
+      'nc-microscope': projectTypeName === 'Research',
+      'nc-settings-91': projectTypeName === 'MFG',
+      'nc-code-editor': projectTypeName === 'Program',
+      'nc-book-open-2': projectTypeName === 'Solution',
+      'nc-board-28': projectTypeName === 'Initiative',
+      'nc-bulb-63': projectTypeName === 'TOF Engaged',
+      'nc-sign-closed': projectTypeName === 'Completed',
+      'nc-gantt': projectTypeName === 'General'
+    };
+    return classes;
+  }
+
+  setProjctTypeColor(projectTypeName) {
+    switch (projectTypeName) {
+      case 'NCI':
+        return 'rgb(139, 0, 0)';  // red
+      case 'NPI':
+        return 'rgb(0, 0, 139)';  // dark blue
+      case 'NPPI':
+        return 'rgb(16, 140, 160)';  // turquiose
+      case 'NTI':
+        return 'rgb(215, 123, 10)';  // orange
+      case 'Research':
+        return 'rgb(0, 100, 0)';  // green
+      case 'Initiative':
+        return 'rgb(184, 134, 11)';  // dark yellow-gold
+      case 'General':
+        return 'rgb(0, 101, 209)';  // blue
+      default:
+        return 'rgb(139, 0, 139)';  // magenta
+    }
+  }
+
 
 }
