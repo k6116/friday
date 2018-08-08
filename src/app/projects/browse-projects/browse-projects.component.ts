5333ce21ee02238c18f77c6f347fe5c79a139c8b
SCHUETZLE <bill_schuetzle@keysight.com>
2018-08-08 11:58:06 -0700

    started projects browse page design

--- src/app/projects/browse-projects/browse-projects.component.ts
+++ src/app/projects/browse-projects/browse-projects.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiDataProjectService } from '../../_shared/services/api-data/_index';
 
 @Component({
   selector: 'app-browse-projects',
   templateUrl: './browse-projects.component.html',
-  styleUrls: ['./browse-projects.component.css']
+  styleUrls: ['./browse-projects.component.css', '../../_shared/styles/common.css']
 })
 export class BrowseProjectsComponent implements OnInit {
 
-  constructor() { }
+  projects: any;
+  showPage: boolean;
+
+  constructor(
+    private apiDataProjectService: ApiDataProjectService
+  ) { }
 
   ngOnInit() {
+
+    // get all the projects
+    this.apiDataProjectService.getProjects()
+      .subscribe(
+        res => {
+          console.log('get projects successfull:');
+          console.log(res);
+          this.projects = res;
+          // TEMP CODE: trim down the list of projects to a smaller number, for testing css
+          this.trimProjects(10);
+          // display the page
+          this.showPage = true;
+        },
+        err => {
+          console.log('get project data error:');
+          console.log(err);
+        }
+    );
+
+  }
+
+
+
+  trimProjects(numProjects: number) {
+    this.projects = this.projects.slice(0, numProjects);
   }
 
 }
