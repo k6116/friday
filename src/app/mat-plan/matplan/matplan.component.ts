a235ad4e8b0f6c3104430fff502a8a450c76ab22
Bryan Cheung <bryan.cheung@keysight.com>
2018-10-04 09:04:39 -0700

    added matplan framework

---
+++ src/app/mat-plan/matplan/matplan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-matplan',
+  templateUrl: './matplan.component.html',
+  styleUrls: ['./matplan.component.css', '../../_shared/styles/common.css']
+})
+export class MatplanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
